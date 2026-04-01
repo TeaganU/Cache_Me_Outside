800ca16cc7d36ce7e0b7c002d723623feb51e0be
treeturtle <owenstrolle@gmail.com>
2026-03-31 18:22:47 -0700

    fixed something else

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
 });
 
-dotenv.config();
+dotenv.config({path: "./server/src/.env"});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
